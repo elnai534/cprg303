86e2fe82e4c0a22eb77f9996536cb610c09e6251
elnai534 <152654699+elnai534@users.noreply.github.com>
2024-10-29 14:14:13 -0600

    Add files via upload

---
+++ components/ToDoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View,
+  TextInput,
+  Button
+} from 'react-native';
+
+<View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task..."
+        />
+        <Button title="Add" />
+      </View>
